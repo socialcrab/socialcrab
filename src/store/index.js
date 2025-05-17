1feb63c8064a414e428fa2d77cbae4efa27a5088
socialcrab <backend@socialcrab.id>
2025-05-17 23:49:59 +0700

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,7 @@ import email from 'src/store/apps/email'
 import invoice from 'src/store/apps/invoice'
 import calendar from 'src/store/apps/calendar'
 import permissions from 'src/store/apps/permissions'
+import x from 'src/store/apps/x'
 
 export const store = configureStore({
   reducer: {
@@ -16,7 +17,8 @@ export const store = configureStore({
     email,
     invoice,
     calendar,
-    permissions
+    permissions,
+    x
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
